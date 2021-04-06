c8190e1a1dd4a34934960006041a68252f4b6b99
Александр <64688166+varynskyi@users.noreply.github.com>
2021-04-06 13:52:48 +0300

    Slider-ok

---
+++ src/slide.js
@@ -0,0 +1,29 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+document.getElementById("plusbtn").addEventListener('click', () => { plusSlides(1) });
+document.getElementById("minusbtn").addEventListener('click', () => { plusSlides(-1) });
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+ }
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
